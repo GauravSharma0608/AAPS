e7b0f76a0e2586cdab6b9618caa766b93499808f
Gaurav Sharma <gaursh0608@gmail.com>
2025-06-02 23:12:33 +0530

    Add files via upload

---
+++ Tree/src/Spiral_matrix.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spiral_matrix {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Bottom row
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Left column
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+
+    }
+    public static void main(String[] args) {
+        Spiral_matrix sm = new Spiral_matrix();
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        List<Integer> result = sm.spiralOrder(matrix);
+        System.out.println(result);
+    }
+}
